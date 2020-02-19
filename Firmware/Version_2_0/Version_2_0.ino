adc0bebbad3d4c6b9a18345245faaa1f94124308
Benjamin Voelker <voelkerb@informatik.uni-freiburg.de>
2020-02-19 17:55:24 +0100

    hotfix, OTA not working due to streamserver connect

--- Firmware/Version_2_0/Version_2_0.ino
+++ Firmware/Version_2_0/Version_2_0.ino
@@ -1101,7 +1101,7 @@ void timer_init() {
  ****************************************************/
 TaskHandle_t streamServerTaskHandle = NULL;
 void checkStreamServer(void * pvParameters) {
-  while (true) {
+  while (not updating) {
     if (strcmp(config.streamServer, NO_SERVER) != 0 and !exStreamServer.connected()) {
       exStreamServer.connect(config.streamServer, STANDARD_TCP_SAMPLE_PORT);
     }
